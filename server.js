c2b5c182b8fd8d932078580af1b0252939e64788
SupaFront <bluewotka@gmail.com>
2022-05-11 18:55:57 +0300

    vercel deploy

--- server.js
+++ server.js
@@ -5,7 +5,10 @@ const { DB_HOST, PORT = 3000 } = process.env;
 
 mongoose
   .connect(DB_HOST)
-  .then(() => app.listen(PORT))
+  .then(() => {
+    global.connect = 'connect';
+    app.listen(PORT);
+  })
   .catch(error => {
     console.log(error.message);
     process.exit(1);
